e4ba8a78280294d164e978b12563dff45a620f4c
Samuel Ng <samuelngsh@gmail.com>
2019-02-22 00:09:50 -0800

    Some precalculation stuff

--- src/groundstate-dwave.py
+++ src/groundstate-dwave.py
@@ -17,57 +17,58 @@ from scipy.spatial import distance
 
 import siqadconn
 
-class DBSimConnector:
-    '''This class serves as a connector between the C++ physics engine connector and
-    the AFMMarcus Python classes'''
+class GroundStateQPU:
+    '''Attempt to find the ground state electron configuration of the given DB 
+    configuration.'''
 
     q0 = 1.602e-19
     eps0 = 8.854e-12
     k_b = 8.617e-5
 
-    dbs = []                # list of tuples containing all dbs, (x, y)
+    #dbs = []                # list of tuples containing all dbs, (x, y)
 
-    def parse_cml_args(self):
-        parser = ArgumentParser(description="This script takes the problem "
-                "file and attempts to find the ground state electron "
-                "configuration on D-Wave's QPU.")
-        parser.add_argument(dest="in_file", type=self.file_must_exist,
-                help="Path to the problem file.",
-                metavar="IN_FILE")
-        parser.add_argument(dest="out_file", help="Path to the output file.",
-                metavar="OUT_FILE")
-        self.args = parser.parse_args()
+    def __init__(self, in_file, out_file):
+        self.in_file = in_file
+        self.out_file = out_file
 
-    def file_must_exist(self, fpath):
-        '''Check if input file exists for argument parser'''
-        if not os.path.exists(fpath):
-            raise argparse.ArgumentTypeError("{0} does not exist".format(fpath))
-        return fpath
 
     # Import problem parameters and design from SiQAD Connector
     def init_problem(self):
         '''Read the problem from SiQADConnector.'''
-        self.sqconn = siqadconn.SiQADConnector("Ground State Solver QPU", 
-                self.args.in_file, self.args.out_file)
+        self.sqconn = siqadconn.SiQADConnector('Ground State Solver QPU', 
+                self.in_file, self.out_file)
 
         # retrieve DBs and convert to a format that hopping model takes
+        dbs = []
+        db_scale = 1e-10    # DB locations given in angstrom
         for db in self.sqconn.dbCollection():
-            self.dbs.append((db.x, db.y))
+            dbs.append((db.x*db_scale, db.y*db_scale))
 
+        # retrieve and process simulation parameters
         K_c = 1./(4 * np.pi * float(self.sqconn.getParameter('epsilon_r')) * self.eps0)
-        debye_length = 1e-9 * float(self.sqconn.getParameter('debye_length'))
+        debye_length = float(self.sqconn.getParameter('debye_length'))
+        debye_length *= 1e-9 # debye_length given in nm
 
-        self.dbs = np.asarray(self.dbs)
-        self.db_r = distance(self.dbs, self.dbs, 'euclidean')
-        # TODO distance scale
-        self.V_ij = self.q0 * K_c * np.exp(-db_r/debye_length) / db_r
-        # TODO deal with divide by 0
+        # precompute distances and inter-DB potentials
+        dbs = np.asarray(dbs)
+        db_r = distance.cdist(dbs, dbs, 'euclidean')
+        self.V_ij = np.divide(self.q0 * K_c * np.exp(-db_r/debye_length), 
+                db_r, out=np.zeros_like(db_r), where=db_r!=0)
+
+        print('V_ij=\n{}'.format(self.V_ij))
 
         # TODO prune potentials that are too far away (let user set threshold)
 
         # TODO estimate qubit resource requirement and whether further pruning is required
 
-    def invoke_dwave_simulation(self):
+    def invoke_solver(self):
+        '''Invoke D-Wave's solver using the problem defined in this class. In 
+        the future, add user options for using local classical solver rather 
+        than D-Wave's QPU.'''
+
+        print('To be implemented.')
+
+        return
 
     ## Run simulation
     #def runSimulation(self):
@@ -116,9 +117,28 @@ class DBSimConnector:
     #    mw.animator.start()
     #    sys.exit(app.exec_())
 
-if __name__ == "__main__":
-    # TODO maybe move this to animator.py
-    connector = DBSimConnector()
-    connector.parse_cml_args()
-    connector.init_problem()
-    connector.invoke_dwave_simulation()
+
+def parse_cml_args():
+    '''Parse command-line arguments.'''
+
+    def file_must_exist(fpath):
+        '''Local method checking if input file exists for argument parser.'''
+        if not os.path.exists(fpath):
+            raise argparse.ArgumentTypeError('{0} does not exist'.format(fpath))
+        return fpath
+
+    parser = ArgumentParser(description='This script takes the problem '
+            'file and attempts to find the ground state electron '
+            'configuration on D-Wave\'s QPU.')
+    parser.add_argument(dest='in_file', type=file_must_exist,
+            help='Path to the problem file.',
+            metavar='IN_FILE')
+    parser.add_argument(dest='out_file', help='Path to the output file.',
+            metavar='OUT_FILE')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    cml_args = parse_cml_args()
+    gs_qpu = GroundStateQPU(cml_args.in_file, cml_args.out_file)
+    gs_qpu.init_problem()
+    gs_qpu.invoke_solver()
