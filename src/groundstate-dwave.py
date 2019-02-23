14e4213491aa3d13ce965ea17574a06dcf39487d
Samuel Ng <samuelngsh@gmail.com>
2019-02-22 19:16:33 -0800

    Recalculate system energy after retrieve results from D-Wave

--- src/groundstate-dwave.py
+++ src/groundstate-dwave.py
@@ -43,6 +43,8 @@ class GroundStateQPU:
 
         sq_param = lambda key : self.sqconn.getParameter(key)
 
+        self.repeat_count = int(sq_param('repeat_count'))
+
         # retrieve DBs and convert to a format that hopping model takes
         db_scale = 1e-10            # DB locations given in angstrom
         dbs = [(db.x*db_scale, db.y*db_scale) for db in self.sqconn.dbCollection()]
@@ -56,10 +58,12 @@ class GroundStateQPU:
         # precompute distances and inter-DB potentials
         db_r = distance.cdist(dbs, dbs, 'euclidean')
         d_threshold = 1e-9 * float(sq_param('d_threshold'))
-        db_r[db_r>d_threshold] = 0  # prune elements past distance threshold
         self.V_ij = np.divide(self.q0 * K_c * np.exp(-db_r/debye_length), 
                 db_r, out=np.zeros_like(db_r), where=db_r!=0)
+        self.V_ij_pruned = self.V_ij
+        self.V_ij_pruned[db_r>d_threshold] = 0  # prune elements past distance threshold
         print('V_ij=\n{}'.format(self.V_ij))
+        print('V_ij_pruned=\n{}'.format(self.V_ij_pruned))
 
         # local potentials
         self.V_local = np.ones(len(dbs)) * -1 * float(sq_param('global_v0'))
@@ -73,9 +77,9 @@ class GroundStateQPU:
             key_i = 'db{}'.format(i)
             self.linear[(key_i, key_i)] = self.V_local[i]
             for j in range(i+1,len(db_r[0])):
-                if self.V_ij[i][j] != 0:
+                if self.V_ij_pruned[i][j] != 0:
                     key_j = 'db{}'.format(j)
-                    self.quadratic[(key_i, key_j)] = self.V_ij[i][j]
+                    self.quadratic[(key_i, key_j)] = self.V_ij_pruned[i][j]
 
         print(self.linear)
         print(self.quadratic)
@@ -91,7 +95,7 @@ class GroundStateQPU:
         Q = dict(self.linear)
         Q.update(self.quadratic)
 
-        self.response = EmbeddingComposite(DWaveSampler()).sample_qubo(Q, num_reads=1000)
+        self.response = EmbeddingComposite(DWaveSampler()).sample_qubo(Q, num_reads=self.repeat_count)
 
         for datum in self.response.data(['sample', 'energy', 'num_occurrences']):
             print(datum.sample, datum.energy, "Occurrences: ", datum.num_occurrences)
@@ -126,11 +130,24 @@ class GroundStateQPU:
             charge_config = ''
             for charge in datum.sample.values():
                 charge_config += str(charge)
-            charge_configs.append([charge_config, str(datum.energy), str(datum.num_occurrences)])
+            charge_configs.append([charge_config, str(self.system_energy(charge_config)), str(datum.num_occurrences)])
         print(charge_configs)
 
         self.sqconn.export(db_loc=dblocs)
         self.sqconn.export(db_charge=charge_configs)
+
+    def system_energy(self, charge_config):
+        '''Return the system energy of the given charge configuration 
+        accounting for all Coulombic interactions.'''
+
+        E = 0.
+        charges = [int(c) for c in charge_config]
+        
+        for i in range(len(charges)):
+            for j in range(i+1,len(charges)):
+                E += self.V_ij[i][j] * charges[i] * charges[j]
+
+        return E
 
     ## Run simulation
     #def runSimulation(self):
